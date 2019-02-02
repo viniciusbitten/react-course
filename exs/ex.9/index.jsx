caf59e36f853aa56596504f245c8411d9560bab2
Vinicius <viniciusbittencourt@me.com>
2019-02-02 16:06:04 -0200

    Exercise - ReactRedux

--- exs/ex.9/index.jsx
+++ exs/ex.9/index.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {combineReducers, createStore} from 'redux'
 import {Provider} from 'react-redux'
-import Counter from './counter'
-import counterReducer from './counterReducer';
+import Field from './field'
+import fieldReducer from './fieldReducer'
 
 const reducers = combineReducers({
-    counter: counterReducer
+    field: fieldReducer
 })
 
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
-        <Counter/>
+        <Field/>
     </Provider>
 
 , document.getElementById('app'))
